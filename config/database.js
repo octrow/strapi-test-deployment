a6e45259e88bb7e303fd1ade2adef8454109e955
octrow <octrow@gmail.com>
2022-10-20 06:06:58 +0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'containers-us-west-70.railway.app'),
-      port: env.int('DATABASE_PORT', 7477),
+      host: env('DATABASE_HOST', 'containers-us-west-69.railway.app'),
+      port: env.int('DATABASE_PORT', 7687),
       database: env('DATABASE_NAME', 'railway'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'ch047CrvDxBbGVFAzsx7'),
+      password: env('DATABASE_PASSWORD', 'WRC9J5jmvRXMcIY3YitT'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
